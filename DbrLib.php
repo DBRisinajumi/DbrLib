d4de5e28e421669f5da633b8d5421561cf581ce2
edmundspriede <edmunds@kls.lv>
2013-10-19 15:14:04 +0300

    fix

--- DbrLib.php
+++ DbrLib.php
@@ -21,32 +21,32 @@ class DbrLib{
                  break;  
                  case 'yesterday' :
                    
-                     $criteria->addCondition("'$fieldname' = DATE_SUB(DATE(NOW()), INTERVAL 1 DAY)" );  // date is database date column field
+                     $criteria->addCondition("$fieldname = DATE_SUB(DATE(NOW()), INTERVAL 1 DAY)" );  // date is database date column field
                  
                  break;  
                  case 'thisweek' :
                    
-                     $criteria->addCondition("WEEK('$fieldname') = WEEK(NOW()) AND YEAR('$fieldname')= YEAR(NOW())" );  // date is database date column field
+                     $criteria->addCondition("WEEK($fieldname) = WEEK(NOW()) AND YEAR($fieldname)= YEAR(NOW())" );  // date is database date column field
                  
                  break;  
                  case 'lastweek' :
                    
-                     $criteria->addCondition("WEEK('$fieldname') = WEEK(NOW()-1)AND YEAR('$fieldname')= YEAR(NOW())" );  // date is database date column field
+                     $criteria->addCondition("WEEK($fieldname) = WEEK(NOW()-1)AND YEAR($fieldname)= YEAR(NOW())" );  // date is database date column field
                  
                  break; 
                  case 'thismonth' :
                    
-                     $criteria->addCondition("MONTH('$fieldname') = MONTH(NOW()) AND YEAR('$fieldname')= YEAR(NOW())" );  // date is database date column field
+                     $criteria->addCondition("MONTH($fieldname) = MONTH(NOW()) AND YEAR($fieldname)= YEAR(NOW())" );  // date is database date column field
                  
                  break; 
                  case 'lastmonth' :
                    
-                     $criteria->addCondition("MONTH('$fieldname') = MONTH(NOW()-1) AND YEAR('$fieldname')= YEAR(NOW())" );  // date is database date column field
+                     $criteria->addCondition("MONTH($fieldname) = MONTH(NOW()-1) AND YEAR($fieldname)= YEAR(NOW())" );  // date is database date column field
                  
                  break;
                   case 'thisyear' :
                    
-                     $criteria->addCondition("YEAR('$fieldname') =  YEAR(NOW())" );  // date is database date column field
+                     $criteria->addCondition("YEAR($fieldname) =  YEAR(NOW())" );  // date is database date column field
                  
                  break;
              }
